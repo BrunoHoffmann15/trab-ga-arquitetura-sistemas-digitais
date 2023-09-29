65302b7a21847639ff5bd7e8844070a405d10d7a
BrunoHoffmann15 <bruno.siqueira.hoffmann@gmail.com>
2023-09-29 09:17:10 -0300

    feat: adicionando novas cenários

--- tests/ula_8_bits_testbench.sv
+++ tests/ula_8_bits_testbench.sv
@@ -3,7 +3,7 @@ module test_bench_ula_8_bits_structure ();
 	reg [7:0]A;
 	reg [7:0]B;
 	reg [2:0]X;
-  	reg [7:0]EXPECTED;
+  reg [7:0]EXPECTED;
 	wire [7:0]S;
 	wire COUT;
 
@@ -19,6 +19,7 @@ module test_bench_ula_8_bits_structure ();
 		// A = 1000 0011
 		// B = 0000 0001
 		// X = 000 -> FULL ADDER
+		// Expected = 1000 0100
 
 		CIN=0;X=3'b000;A=8'b10000011;B=8'b00000001;EXPECTED=8'b10000100;#10
 
@@ -32,6 +33,7 @@ module test_bench_ula_8_bits_structure ();
 		// A = 1000 0011
 		// B = 0000 0001
 		// X = 001 -> FULL SUBTRACTOR
+		// Expected = 1000 0010
 
 		CIN=0;X=3'b001;A=8'b10000011;B=8'b00000001;EXPECTED=8'b10000010;#10
 		$display("Simulation 2: Option=%b; A=%b %b; B=%b %b; Subtract=%b %b;", X, A[7:4], A[3:0], B[7:4], B[3:0], S[7:4], S[3:0]);
@@ -44,6 +46,7 @@ module test_bench_ula_8_bits_structure ();
 		// A = 1000 0011
 		// B = 0000 0001
 		// X = 010 -> AND
+		// Expected = 0000 0001
 
 		CIN=0;X=3'b010;A=8'b10000011;B=8'b00000001;EXPECTED=8'b00000001;#10
 		$display("Simulation 3: Option=%b; A=%b %b; B=%b %b; And=%b %b;", X, A[7:4], A[3:0], B[7:4], B[3:0], S[7:4], S[3:0]);
@@ -52,7 +55,7 @@ module test_bench_ula_8_bits_structure ();
 			$display("Failed on scenario 3 should be %b %b.", EXPECTED[7:4], EXPECTED[3:0]);
 		end
 
-		// Simulation 1
+		// Simulation 4
 		// A = 1000 0011
 		// B = 0000 0001
 		// X = 011 -> OR
@@ -64,15 +67,43 @@ module test_bench_ula_8_bits_structure ();
 			$display("Failed on scenario 4 should be %b %b.", EXPECTED[7:4], EXPECTED[3:0]);
 		end
 
-		// Simulation 1
+		// Simulation 5
 		// A = 1000 0011
 		// X = 100 -> NOT
+		// Expected = 0111 1100
 
 		CIN=0;X=3'b100;A=8'b10000011;EXPECTED=8'b01111100;#10
 		$display("Simulation 5: Option=%b; A=%b %b; Not=%b %b;", X, A[7:4], A[3:0], S[7:4], S[3:0]);
 
 		if(EXPECTED != S) begin
 			$display("Failed on scenario 5 should be %b %b.", EXPECTED[7:4], EXPECTED[3:0]);
+		end
+
+		// Simulation 6
+		// A = 0001 0011
+		// B = 0001 0001
+		// X = 000 -> FULL ADDER
+		// Expected = 0010 0100
+
+		CIN=0;X=3'b000;A=8'b00010011;B=8'b00010001;EXPECTED=8'b00100100;#10
+
+		$display("Simulation 6: Option=%b; A=%b %b; B=%b %b; Add=%b %b;", X, A[7:4], A[3:0], B[7:4], B[3:0], S[7:4], S[3:0]);
+
+		if(EXPECTED != S) begin
+			$display("Failed on scenario 6 should be %b %b.", EXPECTED[7:4], EXPECTED[3:0]);
+		end
+
+		// Simulation 7
+		// A = 1001 0010
+		// B = 0000 0110
+		// X = 001 -> FULL SUBTRACTOR
+		// Expected = 1000 1100
+
+		CIN=0;X=3'b001;A=8'b10010010;B=8'b00000110;EXPECTED=8'b10001100;#10
+		$display("Simulation 7: Option=%b; A=%b %b; B=%b %b; Subtract=%b %b;", X, A[7:4], A[3:0], B[7:4], B[3:0], S[7:4], S[3:0]);
+
+		if(EXPECTED != S) begin
+			$display("Failed on scenario 7 should be %b %b.", EXPECTED[7:4], EXPECTED[3:0]);
 		end
 	end
 endmodule
